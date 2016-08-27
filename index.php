b20ffbded7cbb8d63b09b3a6e6aee985779e48e3
AMiT Kumar <dtu.amit@gmail.com>
2016-08-27 13:23:48 +0530

    simple index.php

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello, Homepage!");
 
     return $response;
 });
+
+
 $app->run();
 

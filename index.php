338fce6e5e8030ea07604c1cbe0862409d2bc5dc
AMiT Kumar <dtu.amit@gmail.com>
2016-08-27 13:56:04 +0530

    Add code for twig

--- index.php
+++ index.php
@@ -4,7 +4,18 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+);
+
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
 
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello, Homepage!");
